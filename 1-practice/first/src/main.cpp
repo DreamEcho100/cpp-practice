cde552d5a8ccf71f4bd77358ea5bf1c849053a34
DreamEcho100 <maze6572198@gmail.com>
2024-02-05 02:30:07 +0200

    Refactor main.cpp file

--- 1-practice/first/src/main.cpp
+++ 1-practice/first/src/main.cpp
@@ -2,41 +2,44 @@
 #include <string>
 #include <.gitignore>
 
-void FancyLog(const char* message);
+void FancyLog(const char *message);
 
-int main() {
-		std::cout << "Hello, World!" << std::endl;
-		FancyLog("Hello, World!");
+int main()
+{
+	std::cout << "Hello, World!" << std::endl;
+	FancyLog("Hello, World!");
 
-		std::string str = "lol";
-		int strLength = str.length();
+	std::string str = "lol";
+	int strLength = str.length();
 
-		std::cout << "strLength: " << strLength << "\n";
+	std::cout << "strLength: " << strLength << "\n";
 
-		int i;
-		
-		while (true)
+	int i;
+
+	while (true)
+	{
+		std::cout << "Enter a number (between 0 and " << strLength - 1 << "): \n";
+		// i = std::cin.get();
+		std::cin >> i;
+		std::cout << "i: " << i << "\n";
+		if (i >= 0 && i < strLength)
 		{
-			std::cout << "Enter a number (between 0 and " << strLength - 1 << "): \n";
-			// i = std::cin.get();
-			std::cin >> i;
-			std::cout << "i: " << i << "\n";
-			if (i >= 0 && i < strLength)	{
-				break;
-			}	else {
-				std::cout << "Invalid number. Try again.\n";
-			}
+			break;
 		}
-		
+		else
+		{
+			std::cout << "Invalid number. Try again.\n";
+		}
+	}
 
-		char character = str[i];
-		short shorty = character;
-		std::cout
+	char character = str[i];
+	short shorty = character;
+	std::cout
 			<< "str: " << str << "\n"
 			<< "character: " << character << "\n"
 			<< "shorty: " << shorty << "\n";
 
-		std::cin.get();
+	std::cin.get();
 
-		return 0;
+	return 0;
 }
