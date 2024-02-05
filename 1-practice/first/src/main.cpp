5af451c65cb7a133f9ac89c29c9f8cea3109a2d2
DreamEcho100 <maze6572198@gmail.com>
2024-02-05 02:25:14 +0200

    Add file associations and update main.cpp

--- 1-practice/first/src/main.cpp
+++ 1-practice/first/src/main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include <string>
+#include <.gitignore>
 
 void FancyLog(const char* message);
 
 int main() {
 		std::cout << "Hello, World!" << std::endl;
 		FancyLog("Hello, World!");
+
+		std::string str = "lol";
+		int strLength = str.length();
+
+		std::cout << "strLength: " << strLength << "\n";
+
+		int i;
+		
+		while (true)
+		{
+			std::cout << "Enter a number (between 0 and " << strLength - 1 << "): \n";
+			// i = std::cin.get();
+			std::cin >> i;
+			std::cout << "i: " << i << "\n";
+			if (i >= 0 && i < strLength)	{
+				break;
+			}	else {
+				std::cout << "Invalid number. Try again.\n";
+			}
+		}
+		
+
+		char character = str[i];
+		short shorty = character;
+		std::cout
+			<< "str: " << str << "\n"
+			<< "character: " << character << "\n"
+			<< "shorty: " << shorty << "\n";
+
+		std::cin.get();
+
 		return 0;
 }
